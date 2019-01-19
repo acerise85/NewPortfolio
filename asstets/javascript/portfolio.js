c32f395b642b7526f789b768470150f3dc187cb1
acerise <42525915+acerise85@users.noreply.github.com>
2019-01-19 16:12:25 -0700

    Working links, styled social media

--- asstets/javascript/portfolio.js
+++ asstets/javascript/portfolio.js
@@ -1,34 +1,16 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-    // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCppdm_QHNWKwtumoe5OGCxW4ykv9bCdME",
-    authDomain: "myprojects-9e249.firebaseapp.com",
-    databaseURL: "https://myprojects-9e249.firebaseio.com",
-    projectId: "myprojects-9e249",
-    storageBucket: "",
-    messagingSenderId: "365998923166"
-  };
-  firebase.initializeApp(config);
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
 
-    let projects = {
-       projectURL: "https://acerise85.github.io/unit-4-game/",
-       projectName: "",
-       porjectImage: ,
-        
-      
-    };
+  // Or with jQuery
 
-    //Logging the trains in the databse and taking a snapshot for persistance of data
-    database.ref('/projects').on("child_added", function (childSnapshot) {
+  $(document).ready(function () {
+    $('.sidenav').sidenav();
+  });
 
-    // Log everything that's coming out of snapshot
-    var projects = childSnapshot.val().format;
-   
-    //Displaying train information to DOM
-    
-
-  })
 
 
 

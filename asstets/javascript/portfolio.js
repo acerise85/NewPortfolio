d2585a8645647ec6281d2ffbfa2d9c510830a346
acerise <42525915+acerise85@users.noreply.github.com>
2019-01-23 19:43:42 -0700

    mobile ready

--- asstets/javascript/portfolio.js
+++ asstets/javascript/portfolio.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+
+  
+    
+
   document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
